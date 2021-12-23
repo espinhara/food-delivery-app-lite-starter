0f9f2235cc4f134add89898cdf39f8459bdb4d00
Gabriel Espinhara Da Silva <gabrielespinhara13@gmail.com>
2021-12-22 22:26:32 -0300

    Create Tab Button Footer Component

--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -39,11 +39,180 @@ import {
     Header
 } from "../components";
 
+const TabButton = ({
+    label, 
+    icon, 
+    isFocused, 
+    onPress, 
+    outerContainerStyle,
+    innerContaionerStyle
+}) => {
+    return(
+        <TouchableWithoutFeedback
+            onPress={onPress}
+        >
+            <Animated.View
+                style={[
+                    {
+                        flex: 1,
+                        alignItems:'center',
+                        justifyContent:'center'
+                    },
+                    outerContainerStyle
+                ]}
+            >
+                <Animated.View
+                    style={[
+                        {
+                            flexDirection:'row',
+                            width: "80%",
+                            height: "50%",
+                            alignItems:"center",
+                            justifyContent:'center', 
+                            borderRadius:25
+                        },
+
+                        innerContaionerStyle
+                    ]}
+                >
+                    <Image
+                        source={icon}
+                        style={{
+                            width: 20,
+                            height: 20,
+                            tintColor:isFocused ? COLORS.white : COLORS.gray,
+                        }}
+                    />
+                    {isFocused &&
+                        <Text
+                            numberOfLines={1}
+                            style={{
+                                marginLeft: SIZES.base,
+                                color: COLORS.white,
+                                ...FONTS.h3
+                            }}
+                        >
+                            {label}
+                        </Text>
+                    }
+                </Animated.View>
+            </Animated.View>
+        </TouchableWithoutFeedback>
+    )
+}
+
 
 const MainLayout = ({ drawerAnimationStyle, navigation, selectedTab, setSelectedTab }) => {
+    
+    // Reanimated Shared Value
+    const homeTabFlex = useSharedValue(1)
+    const homeTabColor = useSharedValue(COLORS.white)
+    const searchTabFlex = useSharedValue(1)
+    const searchTabColor = useSharedValue(COLORS.white)
+    const cartTabFlex = useSharedValue(1)
+    const cartTabColor = useSharedValue(COLORS.white)
+    const favouriteTabFlex = useSharedValue(1)
+    const favouriteTabColor = useSharedValue(COLORS.white)
+    const notificationTabFlex = useSharedValue(1)
+    const notificationTabColor = useSharedValue(COLORS.white)
+
+    // Reanimated Animated Style
+    const homeFlexStyle = useAnimatedStyle(()=>{
+        return{
+            flex: homeTabFlex.value,
+        }
+    })
+    const homeColorStyle = useAnimatedStyle(()=>{
+        return{
+            backgroundColor: homeTabColor.value,
+        }
+    })
+    const searchFlexStyle = useAnimatedStyle(()=>{
+        return{
+            flex: searchTabFlex.value,
+        }
+    })
+    const searchColorStyle = useAnimatedStyle(()=>{
+        return{
+            backgroundColor: searchTabColor.value,
+        }
+    })
+    const cartFlexStyle = useAnimatedStyle(()=>{
+        return{
+            flex: cartTabFlex.value,
+        }
+    })
+    const cartColorStyle = useAnimatedStyle(()=>{
+        return{
+            backgroundColor: cartTabColor.value,
+        }
+    })
+    const favouriteFlexStyle = useAnimatedStyle(()=>{
+        return{
+            flex: favouriteTabFlex.value,
+        }
+    })
+    const favouriteColorStyle = useAnimatedStyle(()=>{
+        return{
+            backgroundColor: favouriteTabColor.value,
+        }
+    })
+    const notificationFlexStyle = useAnimatedStyle(()=>{
+        return{
+            flex: notificationTabFlex.value,
+        }
+    })
+    const notificationColorStyle = useAnimatedStyle(()=>{
+        return{
+            backgroundColor: notificationTabColor.value,
+        }
+    })
+
     React.useEffect(()=>{
         setSelectedTab(constants.screens.home)
-    })
+    }, [])
+   
+    React.useEffect(()=>{
+        if(selectedTab == constants.screens.home){
+            homeTabFlex.value = withTiming(4, {duration: 500}),
+            homeTabColor.value = withTiming(COLORS.primary, {duration: 500})
+        }else{
+            homeTabFlex.value = withTiming(1, {duration: 500}),
+            homeTabColor.value = withTiming(COLORS.white, {duration: 500})
+        }
+
+        if(selectedTab == constants.screens.search){
+            searchTabFlex.value = withTiming(4, {duration: 500}),
+            searchTabColor.value = withTiming(COLORS.primary, {duration: 500})
+        }else{
+            searchTabFlex.value = withTiming(1, {duration: 500}),
+            searchTabColor.value = withTiming(COLORS.white, {duration: 500})
+        }
+        
+        if(selectedTab == constants.screens.cart){
+            cartTabFlex.value = withTiming(4, {duration: 500}),
+            cartTabColor.value = withTiming(COLORS.primary, {duration: 500})
+        }else{
+            cartTabFlex.value = withTiming(1, {duration: 500}),
+            cartTabColor.value = withTiming(COLORS.white, {duration: 500})
+        }
+
+        if(selectedTab == constants.screens.favourite){
+            favouriteTabFlex.value = withTiming(4, {duration: 500}),
+            favouriteTabColor.value = withTiming(COLORS.primary, {duration: 500})
+        }else{
+            favouriteTabFlex.value = withTiming(1, {duration: 500}),
+            favouriteTabColor.value = withTiming(COLORS.white, {duration: 500})
+        }
+
+        if(selectedTab == constants.screens.notification){
+            notificationTabFlex.value = withTiming(4, {duration: 500}),
+            notificationTabColor.value = withTiming(COLORS.primary, {duration: 500})
+        }else{
+            notificationTabFlex.value = withTiming(1, {duration: 500}),
+            notificationTabColor.value = withTiming(COLORS.white, {duration: 500})
+        }
+    }, [selectedTab])
     return (
         <Animated.View
             style={{
@@ -120,7 +289,7 @@ const MainLayout = ({ drawerAnimationStyle, navigation, selectedTab, setSelected
                     end={{x:0, y:4}}
                     colors={[
                         COLORS.transparent,
-                        COLORS.lightGray1
+                        COLORS.gray
                     ]}
                     style={{
                         position:'absolute',
@@ -132,8 +301,6 @@ const MainLayout = ({ drawerAnimationStyle, navigation, selectedTab, setSelected
                         borderTopRightRadius:15
                     }}
                 />
-
-
                 {/* Tabs */}
                 <View
                     style={{
@@ -146,7 +313,56 @@ const MainLayout = ({ drawerAnimationStyle, navigation, selectedTab, setSelected
                         backgroundColor: COLORS.white
                     }}
                 >
-
+                    <TabButton
+                        label={constants.screens.home}
+                        icon={icons.home}
+                        isFocused={selectedTab == constants.screens.home}
+                        outerContainerStyle={homeFlexStyle}
+                        innerContaionerStyle={homeColorStyle}
+                        onPress={
+                            ()=> setSelectedTab(constants.screens.home)
+                        }
+                    />
+                    <TabButton
+                        label={constants.screens.search}
+                        icon={icons.search}
+                        isFocused={selectedTab == constants.screens.search}
+                        outerContainerStyle={searchFlexStyle}
+                        innerContaionerStyle={searchColorStyle}
+                        onPress={
+                            ()=> setSelectedTab(constants.screens.search)
+                        }
+                    />
+                    <TabButton
+                        label={constants.screens.cart}
+                        icon={icons.cart}
+                        isFocused={selectedTab == constants.screens.cart}
+                        outerContainerStyle={cartFlexStyle}
+                        innerContaionerStyle={cartColorStyle}
+                        onPress={
+                            ()=> setSelectedTab(constants.screens.cart)
+                        }
+                    />
+                    <TabButton
+                        label={constants.screens.favourite}
+                        icon={icons.favourite}
+                        isFocused={selectedTab == constants.screens.favourite}
+                        outerContainerStyle={favouriteFlexStyle}
+                        innerContaionerStyle={favouriteColorStyle}
+                        onPress={
+                            ()=> setSelectedTab(constants.screens.favourite)
+                        }
+                    />
+                    <TabButton
+                        label={constants.screens.notification}
+                        icon={icons.notification}
+                        isFocused={selectedTab == constants.screens.notification}
+                        outerContainerStyle={notificationFlexStyle}
+                        innerContaionerStyle={notificationColorStyle}
+                        onPress={
+                            ()=> setSelectedTab(constants.screens.notification)
+                        }
+                    />
                 </View>
             </View>
 
