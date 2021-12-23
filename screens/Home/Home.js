c9ac26ab2c291972f3b1720f2f111deb14e78ab3
Gabriel Espinhara Da Silva <gabrielespinhara13@gmail.com>
2021-12-23 20:04:40 -0300

    add search bar

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -2,18 +2,85 @@ import React from 'react';
 import {
     View,
     Text,
+    TouchableOpacity,
+    Image,
+    TextInput,
+    FlatList,
 } from 'react-native';
 
+import {
+    FONTS,
+    SIZES,
+    COLORS,
+    icons,
+    dummyData,
+    images,
+} from "../../constants";
+
+
+
 const Home = () => {
+    function renderSearch(){
+        return(
+            <View
+                style={{
+                    flexDirection:'row',
+                    height: 40,
+                    alignItems:'center',
+                    marginHorizontal: SIZES.padding,
+                    marginVertical: SIZES.base,
+                    paddingHorizontal: SIZES.radius,
+                    borderRadius: SIZES.radius,
+                    backgroundColor: COLORS.lightGray2
+                }}
+            >
+                {/* Icon */}
+                <Image
+                    source={icons.search}
+                    style={{
+                        height: 20,
+                        width: 20,
+                        tintColor: COLORS.black
+                    }}
+                />
+                {/* Text Input */}
+                <TextInput
+                    style={{
+                        flex: 1,
+                        marginLeft:SIZES.radius,
+                        ...FONTS.body3,
+                        // top: 1,
+                    }}
+                    placeholder='search food'
+                />
+                {/* Filter Button */}
+                <TouchableOpacity
+                    // onPress={}
+                >
+                    <Image
+                        source={icons.filter}
+                        style={{
+                            height: 20,
+                            width: 20,
+                            tintColor: COLORS.black
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+        )
+    }
     return (
         <View
             style={{
                 flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
             }}
         >
-            <Text>Home</Text>
+            {/* Search */}
+            {renderSearch()}
+
+            
+            {/* List */}
+
         </View>
     )
 }
