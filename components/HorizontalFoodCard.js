82b1c7b480249bf142c1bf51eeb4ee8c140a5be0
Gabriel Espinhara Da Silva <gabrielespinhara13@gmail.com>
2021-12-29 12:30:41 -0300

    create and export component Horizontal Food Card

---
+++ components/HorizontalFoodCard.js
@@ -0,0 +1,94 @@
+//import liraries
+import React, { Component } from 'react';
+import { 
+    TouchableOpacity,
+    Image,
+    View, 
+    Text, 
+} from 'react-native';
+
+import { COLORS, FONTS, SIZES, icons, images } from "../constants";
+// create a component
+const HorizontalFoodCard = ({containerStyle, imageStyle, item, onPress}) =>{
+    return (
+        <TouchableOpacity
+            style={{
+                flexDirection:'row',
+                borderRadius: SIZES.radius,
+                backgroundColor: COLORS.lightGray2,
+                ...containerStyle
+            }}
+            onPress={onPress}
+        >
+            {/* Image */}
+            <Image
+                source={item.image}
+                style={imageStyle}
+            />
+            {/* Info */}
+            <View
+                style={{
+                    flex: 1,
+                }}
+            >
+                {/* Name */}
+                <Text
+                    style={{
+                        ...FONTS.h3,
+                        fontSize:17,
+                    }}
+                >
+                    {item.name}
+                </Text>
+                {/* Description */}
+                <Text
+                    style={{
+                        color: COLORS.darkGray2,
+                        ...FONTS.body4,
+                    }}
+                >
+                    {item.description}
+                </Text>
+                {/* Price */}
+                <Text
+                    style={{
+                        marginTop: SIZES.base,
+                        ...FONTS.h2
+                    }}
+                >
+                    ${item.price}
+                </Text>
+            </View>
+            {/* Calories */}
+            <View 
+                style={{
+                    flexDirection:'row',
+                    position: 'absolute',
+                    top:5,
+                    right: SIZES.radius,
+                }}
+            >
+                <Image
+                    style={{
+                        width: 30,
+                        height: 30,
+                    }}
+                    source={icons.calories}
+                />
+                <Text
+                    style={{
+                        color: COLORS.darkGray2,
+                        ...FONTS.body5
+                    }}
+                >
+                    {item.calories} Calories
+                </Text>
+            </View>
+
+        </TouchableOpacity>
+    );
+
+}
+
+//make this component available to the app
+export default HorizontalFoodCard;
--- components/index.js
+++ components/index.js
@@ -1,5 +1,8 @@
 import Header from "./Header";
 
+import HorizontalFoodCard from "./HorizontalFoodCard";
+
 export{
-    Header
+    Header,
+    HorizontalFoodCard
 }
