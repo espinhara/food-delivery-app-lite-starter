aa682f5d487ae8682927f7ca5bd6ce50078cd88f
Gabriel Espinhara Da Silva <gabrielespinhara13@gmail.com>
2021-12-29 12:31:50 -0300

    Render Menu Types and List Horizontal Food Card

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -17,9 +17,27 @@ import {
     images,
 } from "../../constants";
 
-
+import { HorizontalFoodCard } from "../../components";
 
 const Home = () => {
+
+    const [selectedCategoryId, setSelectedCategoryId] = React.useState(1);
+    const [selectedMenuType, setSelectedMenuType] = React.useState(1);
+    const [menuList, setMenuList] = React.useState([]);
+
+    React.useEffect(()=>{
+        handleChangeCategory(selectedCategoryId, selectedMenuType)
+    },[])
+
+    // Handle Change Category
+    function handleChangeCategory(categoryId, menuTypeId){
+        // Find the menu based on the menuTypeId
+        let selectedMenu = dummyData.menu.find(a=> a.id == menuTypeId)
+
+        // Set the menu based on the categoryId 
+        setMenuList(selectedMenu?.list.filter(a=> a.categories.includes(categoryId)))
+    }
+
     function renderSearch(){
         return(
             <View
@@ -69,6 +87,42 @@ const Home = () => {
             </View>
         )
     }
+    function renderMenuTypes(){
+        return(
+            <FlatList
+                horizontal
+                data={dummyData.menu}
+                keyExtractor={item => `${item.id}`}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{
+                    marginTop:30,
+                    marginBottom:20,
+                }}
+                renderItem={({item, index})=>(
+                    <TouchableOpacity
+                        style={{
+                            marginLeft: SIZES.padding,
+                            marginRight: 
+                                index == dummyData.menu.length -1 ? SIZES.padding : 0
+                        }}
+                        onPress={()=>{
+                            setSelectedMenuType(item.id)
+                            handleChangeCategory(selectedCategoryId, item.id)
+                        }}
+                    >
+                        <Text 
+                            style={{
+                                color: selectedMenuType ==item.id ? COLORS.primary: COLORS.black,
+                                ...FONTS.h3
+                            }}
+                        >
+                            {item.name}
+                        </Text>
+                    </TouchableOpacity>
+                ) }
+            />
+        )
+    }
     return (
         <View
             style={{
@@ -80,6 +134,36 @@ const Home = () => {
 
             
             {/* List */}
+            <FlatList
+                data={menuList}
+                keyExtractor={(item)=> `${item.id}`}
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent={
+                    <View>
+                        {/* Menu Type */}
+                        {renderMenuTypes()}
+                    </View>
+                }
+                renderItem={({item, index})=> {
+                    return(
+                        <HorizontalFoodCard
+                            containerStyle={{
+                                height: 130,
+                                alignItems:'center',
+                                marginHorizontal: SIZES.padding,
+                                marginBottom:SIZES.radius 
+                            }}
+                            imageStyle={{
+                                marginTop:20,
+                                height: 110,
+                                width: 110,
+                            }}
+                            item={item}
+                            onPress={()=> console.log("HorizontalFoodCard")}
+                        />
+                    )
+                }}
+            />
 
         </View>
     )
